30a842fa0d7899329f9595aa4e00edb001468b3f
seiji-nagano <144106275+seiji-nagano@users.noreply.github.com>
2023-10-02 15:35:27 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :real_last_name, presence: true
+  validates :real_first_name, presence: true
+  validates :main_last_name, presence: true
+  validates :main_first_name, presence: true
+  validates :date_of_birth, presence: true
+
+  has_many :items
+  has_many :buys
+
 end
