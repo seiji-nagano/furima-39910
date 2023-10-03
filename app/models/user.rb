eafd24cdebd9b33c16f8cd09aad3e63c1cb0bef9
seiji-nagano <144106275+seiji-nagano@users.noreply.github.com>
2023-10-03 18:36:27 +0900

    ユーザー管理機能の再々修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
     validates :real_first_name, :real_last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい" }
     validates :main_first_name, :main_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
   end
-  validates :password, :password_confirmation,
+  validates :password,
   format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "must include at least one letter and one digit" }
 end
