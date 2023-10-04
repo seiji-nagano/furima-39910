5b3f4bc1c32075b7e2a5a533301cadfd40e9d1a5
seiji-nagano <144106275+seiji-nagano@users.noreply.github.com>
2023-10-04 15:47:25 +0900

    itemsテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  has_one :buy
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :liability_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :price, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
 end
--- db/migrate/20230928071541_create_items.rb
+++ db/migrate/20230928071541_create_items.rb
@@ -1,9 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :text
-      t.text :image
+      t.string :title,             null: false
+      t.text :explanation,         null: false
+      t.integer :category_id       null: false
+      t.integer :situation_id      null: false
+      t.integer :liability_id      null: false
+      t.integer :prefecture_id     null: false
+      t.integer :delivery_days_id  null: false
+      t.integer :price             null: false
+      t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
   end
