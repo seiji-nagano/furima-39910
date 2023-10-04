166ee51c937c15d8d08d7ce8bb27f9e34a3d5628
seiji-nagano <144106275+seiji-nagano@users.noreply.github.com>
2023-10-04 16:53:53 +0900

    商品出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items  = Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :image, :explanation, :conceptcategory_id, :situation_id, :liability_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
   end
 
 end
