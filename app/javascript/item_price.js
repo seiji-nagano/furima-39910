271aebc99796f643ae133e5cc27f45071a0b9e35
seiji-nagano <144106275+seiji-nagano@users.noreply.github.com>
2023-10-18 18:07:11 +0900

    挙動確認後の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,10 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+    const inputValue = parseFloat(priceInput.value);
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    addTaxDom.innerHTML = Math.round(inputValue * 0.1);
     const profit = document.getElementById("profit");
-    profit.innerHTML = Math.floor(inputValue - (inputValue * 0.1))
+    profit.innerHTML = Math.round(inputValue - (inputValue * 0.1));
   })
 });
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,13 +6,12 @@ class OrderForm
     validates :item_id
     validates :user_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :municipalities
     validates :street_address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
     validates :token
   end
-  
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save(params,user_id)
     order = Order.create(item_id: params[:item_id].to_i, user_id: user_id)
